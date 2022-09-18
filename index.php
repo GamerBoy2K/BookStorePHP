75afc83b2561744f3671f1d7228a0b84d4711507
Sounak Pal <sounakpal1@gmail.com>
2022-09-18 18:11:09 +0530

    style and items design

--- index.php
+++ index.php
@@ -3,13 +3,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Book Store</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <?php
-        echo "Checking the PHP"
-    ?>
 
+    <div class="log">
+        <?php
+            echo "Checking the PHP\nHello";
+        ?>
+    </div>
+
+    <div class="banner">
+        <img class="bannerImg" src="./img/banner.jpg" alt="">
+    </div>
+
+    <div class="newBooks">
+        <h3 class="topic">New Books</h3>
+    
+        <div class="bookItems">
+            <div class="bookDetails">
+                <div class="bookImgContainer">
+                    <div class="bookImg">
+                        <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
+                    </div>
+                </div>
+                
+
+                <div class="bookName">
+                    A demo Book : By a not so famous writter
+                </div>
+
+                <div class="bookPrice">
+
+                </div>
+
+            </div>
+            
+        </div>
+    </div>
     
 </body>
 </html>
