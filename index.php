a228a2ea1452d3691273567d1455f468af276ab3
Sounak Pal <sounakpal1@gmail.com>
2022-10-09 20:03:47 +0530

    footer added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Store</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 </head>
 <body>
 
@@ -43,5 +44,23 @@
         </div>
     </div>
     
+
+    <footer>
+        <div class="footer-details">
+            <h3>Sounak Pal</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nostrum vero maiores dolores nulla.</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="footer-end">
+            <p>Copyright &copy; 2022 : <span>Sounak Pal</span></p>
+        </div>
+    </footer>
+
 </body>
 </html>
