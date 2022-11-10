8e62650786aa419b5b7dc17ae90ee1733f37d899
Sounak Pal <sounakpal1@gmail.com>
2022-11-11 02:52:06 +0530

    dbconnect update & userreg progess

---
+++ dbConnect.php
@@ -0,0 +1,16 @@
+<?php 
+
+$serverLocation="localhost";
+$userName='root';
+$password='';
+$databaseName='bookstore';
+
+$conn=mysqli_connect($serverLocation,$userName,$password,$databaseName);
+
+if($conn){
+    echo"Database connection done<br>";
+}else{
+    die("Error" mysqli_cconnect_error());
+}
+
+?>
