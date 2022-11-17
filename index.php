3e3250a45d37ee19b23013ef6ec6907cc85d0dc2
Sounak Pal <sounakpal1@gmail.com>
2022-11-18 02:09:54 +0530

    index page book load

--- index.php
+++ index.php
@@ -14,24 +14,89 @@
 
         <div class="newBooks">
             <h3 class="topic">New Books</h3>
-        
             <div class="bookItems">
+
                 <div class="bookDetails">
                     <div class="bookImgContainer">
                         <div class="bookImg">
                             <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
                         </div>
                     </div>
-                    
-
                     <div class="bookName">
                         A demo Book : By a not so famous writter
                     </div>
-
                     <div class="bookPrice">
-
+                        INR: 100
                     </div>
+                </div>
 
+                <div class="bookDetails">
+                    <div class="bookImgContainer">
+                        <div class="bookImg">
+                            <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
+                        </div>
+                    </div>
+                    <div class="bookName">
+                        Another demo Book : famous writter
+                    </div>
+                    <div class="bookPrice">
+                        INR: 500
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <div class="newBooks">
+            <h3 class="topic">Old Books</h3>
+            <div class="bookItems">
+
+
+            <?php 
+
+                include 'dbConnect.php';
+                $all_books_sql="select * from product";
+                $result=mysqli_query($conn,$all_books_sql);
+
+                while ($row = mysqli_fetch_array($result))  {
+                    echo '
+                    <div class="bookDetails">
+                    <div class="bookImgContainer">
+                        <div class="bookImg">
+                            <img src="./upload/'.$row[3].'" alt="" style="height: 150px; width: 150px;">
+                        </div>
+                    </div>
+                    <div class="bookName">
+                        '.$row[1].'
+                    </div>
+                    <div class="bookPrice">
+                        '.$row[2].'
+                    </div>
+                </div>
+                    ' ;
+                }
+
+            ?>
+                
+
+                <div class="bookDetails">
+                    <div class="bookImgContainer">
+                        <div class="bookImg">
+                            <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
+                        </div>
+                    </div>
+                    <div class="bookName">
+                        Another demo Book : famous writter
+                    </div>
+                    <div class="bookPrice">
+                        INR: 500
+                    </div>
                 </div>
                 
             </div>
