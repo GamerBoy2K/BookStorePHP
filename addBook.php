4c88821577d8bf03e9525e0e1529d0366b7f2a3e
Sounak Pal <sounakpal1@gmail.com>
2022-11-18 00:42:57 +0530

    book add page

---
+++ addBook.php
@@ -0,0 +1,100 @@
+<?php
+    session_start();
+    if($_SESSION["adminLogged"]==true){
+        echo"<h3>Hello Admin ". $_SESSION["username"] ."</h3>";
+    }else{
+        header("location: adminLogin.php");
+        exit;
+    }
+
+?>
+
+
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        include 'dbConnect.php';
+
+        $product_title = $_POST["title"];
+        $product_price = $_POST["product_price"];
+
+        $target_dir = "upload/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+            $insert_sql="INSERT INTO `product` (`product_id`, `title`, `price`, `file_name`) VALUES (NULL, '$product_title', '$product_price', '$target_file' )";
+            $results=mysqli_query($conn,$insert_sql);
+
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+        }
+    }
+?>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+
+        <label for="title">Title</label>
+        <input type="text" placeholder="Enter the book name" id="title" name="title" minlength="3" maxlength="200">
+        
+        <label for="product_price">Price</label>
+        <input type="text" placeholder="Enter the book price" id="product_price" name="product_price" minlength="1" maxlength="10">
+
+        
+        <label for="fileToUpload">Select image to upload:</label>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+
+        <input type="submit" value="Submit data" name="submit">
+    </form>
+</body>
+</html>
