5e31c0fad0ede9d7352a14f378e42a217552c9f5
Sounak Pal <sounakpal1@gmail.com>
2022-11-18 02:23:05 +0530

    dbConnect msg removed

--- dbConnect.php
+++ dbConnect.php
@@ -8,7 +8,7 @@ $databaseName='bookstore';
 $conn=mysqli_connect($serverLocation,$userName,$password,$databaseName);
 
 if($conn){
-    echo"Database connection done<br>";
+    //echo"Database connection done<br>";
 }else{
     die("Error" . mysqli_connect_error());
 }
