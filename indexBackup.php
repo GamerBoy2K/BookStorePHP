79f681623b2fbe355c202b30ed4264652f088ead
Sounak Pal <sounakpal1@gmail.com>
2022-11-20 13:34:59 +0530

    productPage connected

--- indexBackup.php
+++ indexBackup.php
@@ -1,33 +1,7 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Store</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-</head>
-<body>
 
-    <nav>
-        <div class="logo">
-            Phone Shop
-        </div>
+<?php include 'header.php';?>
 
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Category</a>
-                <ul>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Romantic</a></li>
-                    <li><a href="#">Child</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Best Seller</a></li>
-        </ul>
-    </nav>
-
-
+<div class="mainPage" style="padding-top: 80px;">
     <div class="log">
         <?php
             echo "Checking the PHP\nHello";
@@ -40,46 +14,97 @@
 
     <div class="newBooks">
         <h3 class="topic">New Books</h3>
-    
         <div class="bookItems">
+
             <div class="bookDetails">
                 <div class="bookImgContainer">
                     <div class="bookImg">
                         <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
                     </div>
                 </div>
-                
-
                 <div class="bookName">
                     A demo Book : By a not so famous writter
                 </div>
-
                 <div class="bookPrice">
-
+                    INR: 100
                 </div>
+            </div>
 
+            <div class="bookDetails">
+                <div class="bookImgContainer">
+                    <div class="bookImg">
+                        <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
+                    </div>
+                </div>
+                <div class="bookName">
+                    Another demo Book : famous writter
+                </div>
+                <div class="bookPrice">
+                    INR: 500
+                </div>
             </div>
             
         </div>
     </div>
-    
 
-    <footer>
-        <div class="footer-details">
-            <h3>Sounak Pal</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nostrum vero maiores dolores nulla.</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            </ul>
+
+
+
+
+
+
+    <div class="newBooks">
+        <h3 class="topic">Old Books</h3>
+        <div class="bookItems">
+
+
+        <?php 
+
+            include 'dbConnect.php';
+            $all_books_sql="select * from product";
+            $result=mysqli_query($conn,$all_books_sql);
+
+            while ($row = mysqli_fetch_array($result))  {
+                
+                echo '
+                <a href="./productDetails.php?product='.$row[0].'">
+                    <div class="bookDetails">
+                        <div class="bookImgContainer">
+                            <div class="bookImg">
+                                <img src="./'.$row[3].'" alt="" style="height: 150px; width: 150px;">
+                            </div>
+                        </div>
+                        <div class="bookName">
+                            '.$row[1].'
+                        </div>
+                        <div class="bookPrice">
+                            INR: '.$row[2].'
+                        </div>
+                    </div>
+                </a>
+                ' ;
+            }
+
+        ?>
+            
+
+            <div class="bookDetails">
+                <div class="bookImgContainer">
+                    <div class="bookImg" >
+                        <img src="./img/books/void.jpg" alt="" style="height: 150px; width: 150px;">
+                    </div>
+                </div>
+                <div class="bookName">
+                    Another demo Book : famous writter
+                </div>
+                <div class="bookPrice">
+                    INR: 500
+                </div>
+            </div>
+            
         </div>
+    </div>
+</div>
 
-        <div class="footer-end">
-            <p>Copyright &copy; 2022 : <span>Sounak Pal</span></p>
-        </div>
-    </footer>
 
-</body>
-</html>
+<?php include 'footer.php';?>
--- productDetails.php
+++ productDetails.php
@@ -5,9 +5,13 @@
 
 
 <?php
+
+    include 'dbConnect.php';
     if($_SERVER["REQUEST_METHOD"] == "GET"){
         $productID=$_GET['product'];
-        
+        $bookDetailsSQL="SELECT * from `product` where product_id='$productID'";
+        $result=mysqli_query($conn,$bookDetailsSQL);
+        $details=mysqli_fetch_array($result);
     }else{
         echo "Error! Not a Get request";
     }
@@ -52,27 +56,38 @@
 
 </style>
 
-<div class="container">
-    <div class="productImage">
-        <img src="./upload/void.jpg" alt="">
-    </div>
+<?php
 
-    <div class="productInfo">
-        <div class="productTitle">
-            <h2>This is a Demo title of this book</h2>
-        </div>
+    echo'
+    
+        <div class="container">
+            <div class="productImage">
+                <img src="'.$details[3].'" alt="Img">
+            </div>
 
-        <div class="productPrice">
-            <h3>INR: 100.95</h3>
-        </div>
+            <div class="productInfo">
+                <div class="productTitle">
+                    <h2>'.$details[1].'</h2>
+                </div>
 
-        <div class="buttonBuy">
-            <div class="buyNow">
-                Buy Now
+                <div class="productPrice">
+                    <h3>INR: '.$details[2].'</h3>
+                </div>
+
+                <div class="buttonBuy">
+                    <div class="buyNow">
+                        Buy Now
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+    
+    
+    ';
+
+
+?>
+
 
 <?php
     include 'footer.php'
