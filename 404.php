f4a57825b37a5e03326131dd052d0348139b20de
Sounak Pal <sounakpal1@gmail.com>
2022-11-24 19:33:32 +0530

    404 error page added

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php include 'header.php' ?>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+
+    }
+</style>
+
+
+<div class="container">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_kjixtysj.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px;"  loop  autoplay></lottie-player>
+</div>
+
+
+<?php include 'footer.php' ?>
