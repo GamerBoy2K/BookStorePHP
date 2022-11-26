3dfaa3c07bec53ab0cfee88e224270efd4ea4624
Sounak Pal <sounakpal1@gmail.com>
2022-11-27 00:56:11 +0530

    total sold update

--- orderConfirm.php
+++ orderConfirm.php
@@ -25,11 +25,23 @@
 
         $insert_sql="INSERT INTO `order_details` (`user_id`, `product_id`, `shipping_name`, `email_id`, `phone`, `address`, `value`) VALUES ('$userName', '$productID', '$shippingName', '$shippingEmail', '$shippingPhone', '$shippingAddress', '$productPrice')";
         $results=mysqli_query($conn,$insert_sql);
+
+        $update_sql="UPDATE product SET total_sold = total_sold+1 WHERE product_id = $productID;";
+        $updateQuary=mysqli_query($conn,$update_sql);
     }else{
         echo "Error! Not a POST request";
     }
 
 ?>
+
+
+<style>
+    .container{
+        padding-top: 50px;
+        display: flex;
+        justify-content: center;
+    }
+</style>
 
 <div class="container">
     <h1>Order Placed.</h1>
