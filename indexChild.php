3d595705f876047605c9dd8393a2cc43362c8d73
Sounak Pal <sounakpal1@gmail.com>
2022-11-27 17:06:13 +0530

    multi category index added

---
+++ indexChild.php
@@ -0,0 +1,130 @@
+
+<?php include 'header.php';?>
+
+<div class="mainPage" style="padding-top: 80px;">
+    <div class="log">
+        <?php
+            //echo "Checking the PHP\nHello";
+        ?>
+    </div>
+
+    <div class="banner">
+        <img class="bannerImg" src="./img/banner.jpg" alt="">
+    </div>
+
+    <div class="newBooks">
+        <h3 class="topic">New Added Books</h3>
+        <div class="bookItems">
+
+            <?php 
+
+            include 'dbConnect.php';
+            $all_books_sql="select * from product where category=1 ORDER BY date_time DESC LIMIT 6";
+            $result=mysqli_query($conn,$all_books_sql);
+
+            while ($row = mysqli_fetch_array($result))  {
+                
+                echo '
+                <a href="./productDetails.php?product='.$row[0].'">
+                    <div class="bookDetails">
+                        <div class="bookImgContainer">
+                            <div class="bookImg">
+                                <img src="./'.$row[3].'" alt="" style="max-height: 150px; max-width: 150px;">
+                            </div>
+                        </div>
+                        <div class="bookName">
+                            '.$row[1].'
+                        </div>
+                        <div class="bookPrice">
+                            INR: '.$row[2].'
+                        </div>
+                    </div>
+                </a>
+                ' ;
+            }
+
+        ?>
+            
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <div class="newBooks">
+        <h3 class="topic">Most Selling Books</h3>
+        <div class="bookItems">
+
+
+        <?php 
+            $all_books_sql="select * from product where category=1 ORDER BY total_sold DESC LIMIT 6";
+            $result=mysqli_query($conn,$all_books_sql);
+
+            while ($row = mysqli_fetch_array($result))  {
+                
+                echo '
+                <a href="./productDetails.php?product='.$row[0].'">
+                    <div class="bookDetails">
+                        <div class="bookImgContainer">
+                            <div class="bookImg">
+                                <img src="./'.$row[3].'" alt="" style="max-height: 150px; max-width: 150px;">
+                            </div>
+                        </div>
+                        <div class="bookName">
+                            '.$row[1].'
+                        </div>
+                        <div class="bookPrice">
+                            INR: '.$row[2].'
+                        </div>
+                    </div>
+                </a>
+                ' ;
+            }
+
+        ?>
+            
+        </div>
+    </div>
+
+
+    <div class="newBooks">
+        <h3 class="topic">All Books</h3>
+        <div class="bookItems">
+
+
+        <?php 
+            $all_books_sql="select * from product where category=1";
+            $result=mysqli_query($conn,$all_books_sql);
+
+            while ($row = mysqli_fetch_array($result))  {
+                
+                echo '
+                <a href="./productDetails.php?product='.$row[0].'">
+                    <div class="bookDetails">
+                        <div class="bookImgContainer">
+                            <div class="bookImg">
+                                <img src="./'.$row[3].'" alt="" style="max-height: 150px; max-width: 150px;">
+                            </div>
+                        </div>
+                        <div class="bookName">
+                            '.$row[1].'
+                        </div>
+                        <div class="bookPrice">
+                            INR: '.$row[2].'
+                        </div>
+                    </div>
+                </a>
+                ' ;
+            }
+
+        ?>
+            
+        </div>
+    </div>
+</div>
+
+
+<?php include 'footer.php';?>
